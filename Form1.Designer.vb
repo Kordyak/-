f78f9190855e603a68b49a5797e692b4c351c812
kordyak <kordyak@mail.ru>
2024-06-28 14:17:29 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button_stop = New System.Windows.Forms.Button()
+        Me.start = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button_stop
+        '
+        Me.Button_stop.Location = New System.Drawing.Point(303, 135)
+        Me.Button_stop.Name = "Button_stop"
+        Me.Button_stop.Size = New System.Drawing.Size(295, 57)
+        Me.Button_stop.TabIndex = 0
+        Me.Button_stop.Text = "stop"
+        Me.Button_stop.UseVisualStyleBackColor = True
+        '
+        'start
+        '
+        Me.start.Location = New System.Drawing.Point(303, 44)
+        Me.start.Name = "start"
+        Me.start.Size = New System.Drawing.Size(295, 57)
+        Me.start.TabIndex = 1
+        Me.start.Text = "start"
+        Me.start.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(889, 304)
+        Me.Controls.Add(Me.start)
+        Me.Controls.Add(Me.Button_stop)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button_stop As Button
+    Friend WithEvents start As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,68 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Dim cts As CancellationTokenSource
+
+    Private Sub Syop_Click(sender As Object, e As EventArgs) Handles Button_stop.Click
+        cts.Cancel()
+    End Sub
+
+
+    Private Async Sub Start_Click(sender As Object, e As EventArgs) Handles start.Click
+
+        cts = New CancellationTokenSource()
+        Dim token = cts.Token
+
+        Dim t As Task(Of Boolean) = Task.Run(Function() As Boolean
+                                                 Return test1()
+                                                 If token.IsCancellationRequested Then
+                                                     token.ThrowIfCancellationRequested()
+                                                 End If
+                                             End Function, token)
+        Console.WriteLine(" код 1!")
+
+
+        'cts.Cancel()
+        'cts.CancelAfter(1000)
+
+        Try
+
+
+            If Await t Then
+                Console.WriteLine("t вернуло")
+                cts.Cancel()
+            End If
+
+            Console.WriteLine(" код 2!")
+
+        Catch ex As OperationCanceledException
+            Console.WriteLine(" Прервал асинхронный поток!!!!")
+
+        End Try
+
+        Console.WriteLine("А здесь продолжение кода")
+    End Sub
+
+
+    Function test1() As Boolean
+
+        For i = 0 To 5000000000
+            'Console.WriteLine(i & " - Асинхронный поток")
+
+            'If i = 5 Then
+            '    cts.Cancel()
+            'End If
+
+            'Await Task.Delay(1000, ct)
+        Next
+
+
+        Return True
+
+    End Function
+
+
+
+
+End Class
